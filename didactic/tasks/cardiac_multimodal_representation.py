ca628a4a2aeddc520c46d67bb32b7d408dd4fd79
Jeremstym <jeremstym@gmail.com>
2024-07-16 17:52:12 +0200

    Add self to consider hparams as an attribute

--- didactic/tasks/cardiac_multimodal_representation.py
+++ didactic/tasks/cardiac_multimodal_representation.py
@@ -906,7 +906,7 @@ class CardiacMultimodalRepresentationTask(SharedStepsTask):
         # If use custom attention is not enabled, return the raw attention map
         if not self.hparams.use_custom_attention:
             with torch.enable_grad():
-                if hparams.cross_attention:
+                if self.hparams.cross_attention:
                     attention_map = attention_generator.generate_raw_attention_score2(tabular_attrs, time_series_attrs, targets)
                 else:
                     attention_map = attention_generator.generate_raw_attention_score(tabular_attrs, time_series_attrs, targets)
