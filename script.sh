ac35d464e527aeccbbd964aea24278f73594e37c
Jeremstym <jeremstym@gmail.com>
2024-07-23 16:34:52 +0200

    Script bash for IRENE loop

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --partition=electronic
+#SBATCH --job-name=IRENE-loop
+#SBATCH --nodelist=punk
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=2-16:00:00
+#SBATCH --output=/home/stympopper/bash/out/%x-%j.out
+#SBATCH --error=/home/stympopper/bash/out/%x-%j.err
+
+uname -a
+nvidia-smi
+
+for seed in {44..54}; do
+    poetry run didactic-runner 'hydra.run.dir=/home/stympopper/didacticWORKSHOP/irene_ternary-no-ordinal${seed}' +experiment=cardinal/xtab-finetune +trainer.max_epochs=100 'task.predict_losses={ht_severity:{_target_:torch.nn.CrossEntropyLoss}}' exclude_tabular_attrs=[ht_severity,ht_grade,sanity] +seed=$seed task.irene_baseline=True
+done
