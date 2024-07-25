6db06949bef5223232d0b4792670124bfce7a0b4
Jeremstym <jeremstym@gmail.com>
2024-07-25 15:49:57 +0200

    Script sumfusion XTab

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=electronic
-#SBATCH --job-name=LateConcat
+#SBATCH --job-name=SumFusionXtab
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --time=2-16:00:00
@@ -15,6 +15,6 @@ ulimit -n 4096
 for seed in {43..51}; do
     # poetry run didactic-runner 'hydra.run.dir=/home/stympopper/didacticWORKSHOP/irene_ternary-no-echo${seed}' +experiment=cardinal/xtab-finetune +trainer.max_epochs=100 'task.predict_losses={ht_severity:{_target_:torch.nn.CrossEntropyLoss}}' exclude_tabular_attrs=[ht_severity,ht_grade,sanity] +seed=$seed task.irene_baseline=True
     # poetry run didactic-runner 'hydra.run.dir=/home/stympopper/didacticWORKSHOP/cross-xtab-prenorm${seed}' +experiment=cardinal/xtab-finetune +trainer.max_epochs=100 'task.predict_losses={sanity:{_target_:torch.nn.BCELoss}}' exclude_tabular_attrs=[ht_severity,ht_grade,sanity] +seed=$seed task.cross_attention=True task.first_prenormalization=True ckpt=/home/stympopper/didacticJerem/ckpts/xtab.ckpt
-    poetry run didactic-runner 'hydra.run.dir=/home/stympopper/didacticWORKSHOP/late-concat-xtab${seed}' +experiment=cardinal/xtab-finetune +trainer.max_epochs=100 'task.predict_losses={sanity:{_target_:torch.nn.BCELoss}}' exclude_tabular_attrs=[ht_severity,ht_grade,sanity] +seed=$seed task.cross_attention=False task.first_prenormalization=False task.late_concat=True ckpt=/home/stympopper/didacticJerem/ckpts/xtab.ckpt
-
+    # poetry run didactic-runner 'hydra.run.dir=/home/stympopper/didacticWORKSHOP/late-concat-xtab${seed}' +experiment=cardinal/xtab-finetune +trainer.max_epochs=100 'task.predict_losses={sanity:{_target_:torch.nn.BCELoss}}' exclude_tabular_attrs=[ht_severity,ht_grade,sanity] +seed=$seed task.cross_attention=False task.first_prenormalization=False task.late_concat=True ckpt=/home/stympopper/didacticJerem/ckpts/xtab.ckpt
+    poetry run didactic-runner 'hydra.run.dir=/home/stympopper/didacticWORKSHOP/sum-fusion-xtab${seed}' +experiment=cardinal/xtab-finetune +trainer.max_epochs=100 'task.predict_losses={sanity:{_target_:torch.nn.BCELoss}}' exclude_tabular_attrs=[ht_severity,ht_grade,sanity] +seed=$seed task.cross_attention=False task.first_prenormalization=False task.sum_fusion=True ckpt=/home/stympopper/didacticJerem/ckpts/xtab.ckpt
 done
