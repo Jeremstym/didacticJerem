b86fafbdb7e25ec730382787b8ccb9f0477d12e7
Jeremstym <jeremstym@gmail.com>
2024-08-22 17:40:13 +0200

    Rename metric

--- results_analysis.py
+++ results_analysis.py
@@ -12,12 +12,12 @@ def load_results(model_name: str) -> pd.DataFrame:
     results = pd.DataFrame(columns=["Values"])
     for file in files:
         df = pd.read_csv(file, index_col=0) 
-        df = df.loc[["acc", "roc_auc", "pr_auc"]]["ht_severity_prediction"].reset_index()
+        df = df.loc[["acc", "roc_auc", "pr_auc"]]["ht_severity_prediction"].reset_index().rename(columns={"index": "Metric"})
         results = pd.concat([results, df], axis=0)
     print(results.columns)
     print(results)
     results = pd.to_numeric(results["ht_severity_prediction"], downcast="float")
-    return results.groupby(results.index).mean(), results.groupby(results.index).std()
+    return results.groupby("Metric").mean(), results.groupby("Metric").std()
 
 if __name__ == "__main__":
     model_name = "cross-summation-noPE"
