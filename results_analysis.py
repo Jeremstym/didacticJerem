827731a34bab187a3e4c38efe387dd8978c91951
Jeremstym <jeremstym@gmail.com>
2024-08-22 17:25:44 +0200

    Groupby values and compute mean and std

--- results_analysis.py
+++ results_analysis.py
@@ -14,7 +14,7 @@ def load_results(model_name: str) -> pd.DataFrame:
         df = pd.read_csv(file, index_col=0) 
         df = df.loc[["acc", "roc_auc", "pr_auc"]]["ht_severity_prediction"]
         results = pd.concat([results, df], axis=0)
-    return results
+    return results.groupby(results.index).mean(), results.groupby(results.index).std()
 
 if __name__ == "__main__":
     model_name = "cross-summation-noPE"
