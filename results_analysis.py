5ad1e78167c9fcc74087041a6e312fc1fc23e3b4
Jeremstym <jeremstym@gmail.com>
2024-08-22 17:21:51 +0200

    Create result analysor

---
+++ results_analysis.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+from glob import glob
+
+# Load the results in several csv files named 'test_categorical_scores.csv .csv', just keep acc, roc_auc and pr_auc
+# The files are in folder which path is "~/didactcWORKSHOP/name_of_the_model{seed}/predictions"
+
+def load_results(model_name: str) -> pd.DataFrame:
+    path = '/home/stympopper/didacticWORKSHOP/' + model_name + '[0-9][0-9]/predictions/test_categorical_scores.csv'
+    files = glob(path)
+    if not files:
+        raise ValueError("No files found")
+    results = pd.DataFrame()
+    for file in files:
+        df = pd.read_csv(file, index_col=0) 
+        df = df.loc[["acc", "roc_auc", "pr_auc"]]["ht_severity_prediction"]
+        results = pd.concat([results, df], axis=0)
+    return results
+
+if __name__ == "__main__":
+    model_name = "name_of_the_model"
+    results = load_results(model_name)
+    print(results)
