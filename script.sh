ad040657330090c56e1733591a48636a0c6c7c1c
Jeremstym <jeremstym@gmail.com>
2024-09-05 17:51:57 +0200

    Script new env with xtab

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=hard,electronic
-#SBATCH --job-name=new-env-model
+#SBATCH --job-name=new-env-model-xtab
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --time=2-16:00:00
@@ -144,7 +144,7 @@ for seed in {42..51}; do
     # EXPLORATION AFTER THE WORKSHOP
 
     # poetry run didactic-runner 'hydra.run.dir=/home/stympopper/didacticWORKSHOP/tabular-data-test-tok${seed}' +experiment=cardinal/xtab-finetune +trainer.max_epochs=100 'task.predict_losses={ht_severity:{_target_:torch.nn.CrossEntropyLoss}}' exclude_tabular_attrs=[ht_severity,ht_grade,sanity] +seed=$seed task.cross_attention=False task.first_prenormalization=False task/data=tabular
-    poetry run didactic-runner 'hydra.run.dir=/home/stympopper/didacticWORKSHOP/cross-att-newenv${seed}' +experiment=cardinal/xtab-finetune +trainer.max_epochs=100 'task.predict_losses={ht_severity:{_target_:torch.nn.CrossEntropyLoss}}' exclude_tabular_attrs=[ht_severity,ht_grade,sanity] +seed=$seed task.cross_attention=True task.first_prenormalization=True task.use_positional_encoding=False task.contrastive_loss=null
-    poetry run didactic-runner 'hydra.run.dir=/home/stympopper/didacticWORKSHOP/cross-att-newenv-withcontrastive${seed}' +experiment=cardinal/xtab-finetune +trainer.max_epochs=100 'task.predict_losses={ht_severity:{_target_:torch.nn.CrossEntropyLoss}}' exclude_tabular_attrs=[ht_severity,ht_grade,sanity] +seed=$seed task.cross_attention=True task.first_prenormalization=True task.use_positional_encoding=False
+    poetry run didactic-runner 'hydra.run.dir=/home/stympopper/didacticWORKSHOP/cross-att-newenv-xtab${seed}' +experiment=cardinal/xtab-finetune +trainer.max_epochs=100 'task.predict_losses={ht_severity:{_target_:torch.nn.CrossEntropyLoss}}' exclude_tabular_attrs=[ht_severity,ht_grade,sanity] +seed=$seed task.cross_attention=True task.first_prenormalization=True task.use_positional_encoding=False task.contrastive_loss=null ckpt=/home/stympopper/didacticJerem/ckpts/xtab.ckpt
+    poetry run didactic-runner 'hydra.run.dir=/home/stympopper/didacticWORKSHOP/cross-att-newenv-withcontrastive-xtab${seed}' +experiment=cardinal/xtab-finetune +trainer.max_epochs=100 'task.predict_losses={ht_severity:{_target_:torch.nn.CrossEntropyLoss}}' exclude_tabular_attrs=[ht_severity,ht_grade,sanity] +seed=$seed task.cross_attention=True task.first_prenormalization=True task.use_positional_encoding=False ckpt=/home/stympopper/didacticJerem/ckpts/xtab.ckpt
 
 done
