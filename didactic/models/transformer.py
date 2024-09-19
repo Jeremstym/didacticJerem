472b46c5969359c80f8af21bb5dffc1df0a1f8da
Jeremstym <jeremstym@gmail.com>
2024-09-20 00:44:19 +0200

    Condition for post normalization

--- didactic/models/transformer.py
+++ didactic/models/transformer.py
@@ -192,9 +192,13 @@ class FT_Transformer(nn.Module):
                     }
                 )
                 if layer_idx or not self.prenormalization or self.first_prenormalization:
-                    if modality_side == "l":
+                    if not self.prenormalization:
                         layer[f"{modality_side}_attention_normalization"] = get_nn_module(self.attention_normalization)
-                if modality_side == "l":
+                    elif modality_side == "l":
+                        layer[f"{modality_side}_attention_normalization"] = get_nn_module(self.attention_normalization)
+                if not self.prenormalization:
+                    layer[f"{modality_side}_ffn_normalization"] = get_nn_module(self.ffn_normalization)
+                elif modality_side == "l":
                     layer[f"{modality_side}_ffn_normalization"] = get_nn_module(self.ffn_normalization)
         else:
             layer = nn.ModuleDict(
