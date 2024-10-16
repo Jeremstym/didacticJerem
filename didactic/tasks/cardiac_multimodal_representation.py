0131edc30f99efed715cff6983db3f2bd8376bda
Jeremstym <jeremstym@gmail.com>
2024-10-17 00:22:41 +0200

    Set isinstance

--- didactic/tasks/cardiac_multimodal_representation.py
+++ didactic/tasks/cardiac_multimodal_representation.py
@@ -423,8 +423,8 @@ class CardiacMultimodalRepresentationTask(SharedStepsTask):
             # instead of their current null/default values.
             # 1) Convert missing numerical attributes (NaNs) to numbers to avoid propagating NaNs
             # 2) Clip categorical labels to convert indicators of missing data (-1) into valid indices (0)
-            if self.tabular_tokenizer == nn.Identity:
-                tab_attrs_tokens = tabular_attrs.values()
+            if isinstance(self.tabular_tokenizer, nn.Identity):
+                tab_attrs_tokens = torch.cat(num_attrs, cat_attrs, dim=1) # (N, S_tab)
             else:
                 tab_attrs_tokens = self.tabular_tokenizer(
                     x_num=torch.nan_to_num(num_attrs) if num_attrs is not None else None,
