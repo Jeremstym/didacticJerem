de6f59942ea5a2a85015a9dfd07484e670c58f8f
Jeremstym <jeremstym@gmail.com>
2024-10-16 23:54:23 +0200

    Monitor input features length

--- didactic/models/baselines.py
+++ didactic/models/baselines.py
@@ -183,6 +183,8 @@ class TabularMLP(nn.Module):
             dropout: the dropout rate.
         """
         super().__init__()
+        print(f"in_features: {in_features}")
+        raise ValueError("stop here")
         self.layers = nn.ModuleList(
             [
                 nn.Sequential(
