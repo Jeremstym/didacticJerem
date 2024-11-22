2abd8b49c97f184d6cf16a441022bfbd116cab74
Jeremstym <jeremstym@gmail.com>
2024-11-22 17:03:35 +0100

    Script decouping 1st baselie

--- script.sh
+++ script.sh
@@ -303,8 +303,10 @@ for seed in {42..51}; do
 
     # poetry run didactic-runner 'hydra.run.dir=/data/stympopper/didacticWORKSHOP/TEST-LORA-corrected/seed${seed}' +experiment=cardinal/xtab-lora exclude_tabular_attrs=[ht_severity,ht_grade,sanity] seed=$seed task/data=tab-13+time-series ckpt=/home/stympopper/didacticJerem/ckpts/xtab_lora.ckpt
 
-    poetry run didactic-runner 'hydra.run.dir=/data/stympopper/didacticWORKSHOP/TEST-interleaved/seed${seed}' +experiment=cardinal/xtab-interleaved 'task.predict_losses={ht_severity:{_target_:torch.nn.CrossEntropyLoss}}' exclude_tabular_attrs=[ht_severity,ht_grade,sanity] seed=$seed task/data=tab-13+time-series task.model.encoder.freeze_self_attention=False
-    poetry run didactic-runner 'hydra.run.dir=/data/stympopper/didacticWORKSHOP/TEST-interleaved-frozen/seed${seed}' +experiment=cardinal/xtab-interleaved 'task.predict_losses={ht_severity:{_target_:torch.nn.CrossEntropyLoss}}' exclude_tabular_attrs=[ht_severity,ht_grade,sanity] seed=$seed task/data=tab-13+time-series ckpt=/home/stympopper/didacticJerem/ckpts/xtab_selfatt.ckpt
+    # poetry run didactic-runner 'hydra.run.dir=/data/stympopper/didacticWORKSHOP/TEST-interleaved/seed${seed}' +experiment=cardinal/xtab-interleaved 'task.predict_losses={ht_severity:{_target_:torch.nn.CrossEntropyLoss}}' exclude_tabular_attrs=[ht_severity,ht_grade,sanity] seed=$seed task/data=tab-13+time-series task.model.encoder.freeze_self_attention=False
+    # poetry run didactic-runner 'hydra.run.dir=/data/stympopper/didacticWORKSHOP/TEST-interleaved-frozen/seed${seed}' +experiment=cardinal/xtab-interleaved 'task.predict_losses={ht_severity:{_target_:torch.nn.CrossEntropyLoss}}' exclude_tabular_attrs=[ht_severity,ht_grade,sanity] seed=$seed task/data=tab-13+time-series ckpt=/home/stympopper/didacticJerem/ckpts/xtab_selfatt.ckpt
+
+    poetry run didactic-runner 'hydra.run.dir=/data/stympopper/didacticWORKSHOP/TEST-Decoupling/seed${seed}' +experiment=cardinal/xtab-alignment exclude_tabular_attrs=[ht_severity,ht_grade] seed=$seed task/data=tab-13+time-series
 
     # TEST WITHOUT TRAINING
 
