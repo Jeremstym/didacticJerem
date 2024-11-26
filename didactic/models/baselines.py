e29943846264057a6efff5128f813e435f1e85dd
Jeremstym <jeremstym@gmail.com>
2024-11-26 16:32:45 +0100

    Set d_token as attribute

--- didactic/models/baselines.py
+++ didactic/models/baselines.py
@@ -205,6 +205,7 @@ class ConcatMLPDecoupling(nn.Module):
         """
         super().__init__()
 
+        self.d_token = d_token
         self.n_tabular_attrs = n_tabular_attrs
         self.mlp = MLP(3*d_token, out_features=d_token, n_layers=n_mlp_layers, d_token=d_token, dropout=dropout)
         self.tabular_lin_proj = nn.Linear(d_token, 2*d_token)
