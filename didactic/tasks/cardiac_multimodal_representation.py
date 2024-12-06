7776db8c07ccfabfd172105d58848514fac4a9da
Jeremstym <jeremstym@gmail.com>
2024-12-06 14:33:28 +0100

    Config and implement interleaved invert

--- didactic/tasks/cardiac_multimodal_representation.py
+++ didactic/tasks/cardiac_multimodal_representation.py
@@ -312,6 +312,9 @@ class CardiacMultimodalRepresentationTask(SharedStepsTask):
         elif isinstance(self.encoder, didactic.models.transformer.FT_Interleaved_2UniFTs):  # didactic submodule `FT_Interleaved`
             self.nhead = self.hparams.model.encoder.attention_n_heads
             self.separate_modality = False # temporary
+        elif isinstance(self.encoder, didactic.models.transformer.FT_Interleaved_2UniFTs_Inverted):  # didactic submodule `FT_Interleaved`
+            self.nhead = self.hparams.model.encoder.attention_n_heads
+            self.separate_modality = False # temporary
         else:
             raise NotImplementedError(
                 "To instantiate the cardiac multimodal representation task, it is necessary to determine the number of "
