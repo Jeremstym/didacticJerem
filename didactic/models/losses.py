a399e695f8359065170aa952f0e606bfe3fb57a6
Jeremstym <jeremstym@gmail.com>
2024-12-17 17:58:08 +0100

    Remove monitor

--- didactic/models/losses.py
+++ didactic/models/losses.py
@@ -478,9 +478,6 @@ class SupCLIPLoss(nn.Module):
         Returns:
             Scalar loss value.
         """
-        print(f'tab_unique: {tab_unique}')
-        print(f'ts_anchor: {ts_anchor}')
-        print(f'labels: {labels}')
         labels = labels.view(-1, 1)
         label_mask = torch.eq(labels, labels.t()).float().to(labels.device)
         
