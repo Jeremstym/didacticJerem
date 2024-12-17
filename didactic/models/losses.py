8d1de674685fb35a640b7f83f6ce479103c2a752
Jeremstym <jeremstym@gmail.com>
2024-12-17 17:20:10 +0100

    Print all values

--- didactic/models/losses.py
+++ didactic/models/losses.py
@@ -486,8 +486,12 @@ class SupCLIPLoss(nn.Module):
 
         tab_unique = F.normalize(tab_unique, p=2, dim=1)
         ts_anchor = F.normalize(ts_anchor, p=2, dim=1)
+        print(f"tab_unique: {tab_unique}")
+        print(f"ts_anchor: {ts_anchor}")
         similarity = torch.mm(tab_unique, ts_anchor.t())
+        print(f"similarity: {similarity}")
         similarity -= torch.eye(similarity.shape[0]).to(similarity.device) * self.margin
+        print(f"similarity after margin: {similarity}")
         print(f"smiliarity before temp: {similarity}")
         similarity /= self.temperature
         print(f"smiliarity after temp: {similarity}")
