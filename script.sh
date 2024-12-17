fbb9ea2acc2afcda60a22db244ba1e5a38929ab1
Jeremstym <jeremstym@gmail.com>
2024-12-17 20:23:18 +0100

    Syntax

--- script.sh
+++ script.sh
@@ -360,7 +360,7 @@ for seed in {42..51}; do
 
     # ALL INVERTED BELOW
 
-    poetry run  didactic-runner 'hydra.run.dir=/data/stympopper/didacticWORKSHOP/TEST-Decoupling-FT-2UniFTs-interleaved-NTX-SupCLIP/seed${seed}' +experiment=cardinal/xtab-alignment exclude_tabular_attrs=[ht_severity,ht_grade] seed=$seed task/data=tab-13+time-series task/model/encoder=xtab-interleaved-2UniFTs-invert
+    poetry run didactic-runner 'hydra.run.dir=/data/stympopper/didacticWORKSHOP/TEST-Decoupling-FT-2UniFTs-interleaved-NTX-SupCLIP/seed${seed}' +experiment=cardinal/xtab-alignment exclude_tabular_attrs=[ht_severity,ht_grade] seed=$seed task/data=tab-13+time-series task/model/encoder=xtab-interleaved-2UniFTs-invert
 
     # TEST WITHOUT TRAINING
 
