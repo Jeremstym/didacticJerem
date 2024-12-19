2222cdbc44e361c9f2cf73138e5a31ff2ac5301a
Jeremstym <jeremstym@gmail.com>
2024-12-19 14:13:46 +0100

    Change loss name

--- didactic/models/losses.py
+++ didactic/models/losses.py
@@ -591,3 +591,48 @@ class NTXentLoss(nn.Module):
         loss = torch.sum(all_losses) / (2 * batch_size)
 
         return loss
+
+class SupConNTXentLoss(nn.Module):
+    """Normalized Temperature-scaled Cross-Entropy Loss with Decoupling."""
+
+    def __init__(self, temperature: float = 0.1):
+        """Initializes class instance.
+
+        Args:
+            temperature: Temperature scaling factor.
+        """
+        super().__init__()
+        self.temperature = temperature
+
+    def forward(self, tab_unique: Tensor, ts_anchor: Tensor, ts: Tensor, labels: Tensor) -> Tensor:
+        """Performs a forward pass through the loss function.
+
+        Args:
+            tab_unique: (N, E), Embeddings.
+            ts_anchor: (N, E), Embeddings.
+
+        Returns:
+            Scalar loss value.
+        """
+        batch_size = tab_unique.size(0)
+
+        # compute similarity between the embeddings of both views of the data
+        z = torch.cat([tab_unique, ts_anchor], dim=0)
+        similarity = F.cosine_similarity(z.unsqueeze(1), z.unsqueeze(0), dim=2)  # (N * 2, N * 2)
+
+        # Create a mask for the positive samples, i.e. the corresponding samples in each view
+        sim_ij = torch.diag(similarity, batch_size)
+        sim_ji = torch.diag(similarity, -batch_size)
+        positives_mask = torch.cat([sim_ij, sim_ji], dim=0)
+
+        # For the denominator, we need to include both the positive and negative samples, so we use the inverse of the
+        # identity matrix, so that we only exclude the similarities between each embedding and itself
+        pairwise_mask = (~torch.eye(batch_size * 2, batch_size * 2, dtype=torch.bool, device=tab_unique.device)).float()
+
+        numerator = torch.exp(positives_mask / self.temperature)
+        denominator = pairwise_mask * torch.exp(similarity / self.temperature)
+
+        all_losses = -torch.log(numerator / torch.sum(denominator, dim=1))
+        loss = torch.sum(all_losses) / (2 * batch_size)
+
+        return loss
