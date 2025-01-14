d3eec82c140c7d35d503006906d586f063db6b36
Jeremstym <jeremstym@gmail.com>
2025-01-14 14:42:47 +0100

    Add real theoretical infonce loss

--- didactic/models/losses.py
+++ didactic/models/losses.py
@@ -87,6 +87,80 @@ class DecouplingLoss(nn.Module):
 
         return loss
 
+class InfoNCELoss(nn.Module):
+    """Normalized Temperature-scaled Cross-Entropy Loss with Decoupling."""
+
+    def __init__(self, temperature: float = 0.1):
+        """Initializes class instance.
+
+        Args:
+            temperature: Temperature scaling factor.
+        """
+        super().__init__()
+        self.temperature = temperature
+
+    def forward(self, x_unique: Tensor, x_shared: Tensor, ts: Tensor) -> Tensor:
+        """Performs a forward pass through the loss function.
+
+        Args:
+            x_unique: (N, E), Embeddings.
+            x_shared: (N, E), Embeddings.
+
+        Returns:
+            Scalar loss value.
+        """
+        x_unique = F.normalize(x_unique, p=2, dim=1)
+        x_shared = F.normalize(x_shared, p=2, dim=1)
+        ts = F.normalize(ts, p=2, dim=1)
+        sim_shared = torch.mm(x_shared, ts.t())
+        sim_unique = torch.mm(x_unique, ts.t())
+        sim_shared /= self.temperature
+        sim_unique /= self.temperature
+        sim_shared = torch.exp(sim_shared)
+        sim_unique = torch.exp(sim_unique)
+
+        # loss_ts_unique = -torch.log(sim_shared.diag() / torch.sum(sim_unique, dim=1)).mean()
+        loss_nce = -torch.log(sim_shared.diag() / torch.sum(sim_unique, dim=0)).mean()
+
+        return loss
+
+class InfoNCELoss2(nn.Module):
+    """Normalized Temperature-scaled Cross-Entropy Loss with Decoupling."""
+
+    def __init__(self, temperature: float = 0.1):
+        """Initializes class instance.
+
+        Args:
+            temperature: Temperature scaling factor.
+        """
+        super().__init__()
+        self.temperature = temperature
+
+    def forward(self, x_unique: Tensor, x_shared: Tensor, ts: Tensor) -> Tensor:
+        """Performs a forward pass through the loss function.
+
+        Args:
+            x_unique: (N, E), Embeddings.
+            x_shared: (N, E), Embeddings.
+
+        Returns:
+            Scalar loss value.
+        """
+        x_unique = F.normalize(x_unique, p=2, dim=1)
+        x_shared = F.normalize(x_shared, p=2, dim=1)
+        ts = F.normalize(ts, p=2, dim=1)
+        sim_shared = torch.mm(x_shared, ts.t())
+        sim_unique = torch.mm(x_unique, ts.t())
+        sim_shared /= self.temperature
+        sim_unique /= self.temperature
+        sim_shared = torch.exp(sim_shared)
+        sim_unique = torch.exp(sim_unique)
+
+        # loss_ts_unique = -torch.log(sim_shared.diag() / torch.sum(sim_unique, dim=1)).mean()
+        loss_nce = -torch.log(sim_shared.diag() / torch.sum(sim_unique, dim=0) + sim_shared.diag()).mean()
+
+        return loss
+
 class NTXentLossDecoupling2(nn.Module):
     """Normalized Temperature-scaled Cross-Entropy Loss with Decoupling."""
 
