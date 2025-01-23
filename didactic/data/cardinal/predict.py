fc45434b6c4c772ec5f248a1e3b458d62b85d858
Jeremstym <jeremstym@gmail.com>
2025-01-23 14:32:10 +0100

    Increase num iters

--- didactic/data/cardinal/predict.py
+++ didactic/data/cardinal/predict.py
@@ -350,7 +350,7 @@ class CardiacRepresentationPredictionWriter(BasePredictionWriter):
             elif pl_module.hparams.rpr_method == "pacmap":
                 self._embedding_kwargs["n_neighbors"] = 5
                 self._embedding_kwargs["MN_ratio"] = 1.5
-                self._embedding_kwargs["num_iters"] = 70
+                self._embedding_kwargs["num_iters"] = 80
 
             for plot_filename, _ in zip(
                 plots_latent,
