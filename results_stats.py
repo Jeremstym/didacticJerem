5e8f29dd8f410ddf56f0d149e8ec559514550f6d
Jeremstym <jeremstym@gmail.com>
2025-01-24 13:08:11 +0100

    Correct column name

--- results_stats.py
+++ results_stats.py
@@ -26,9 +26,9 @@ def load_results(model_name: str) -> pd.DataFrame:
 @click.option("--model_name", help="Name of the model to load results from")
 @click.option("--model_name2", help="Name of the model to load results from")
 def ttests(model_name: str, model_name2: str) -> pd.DataFrame:
-    results1 = load_results(model_name).drop(columns=["auroc"]).values.tolist()
+    results1 = load_results(model_name).drop(columns=["Metric"]).values.tolist()
     print(f"Results1: {results1}")
-    results2 = load_results(model_name2).drop(columns=["auroc"]).values.tolist()
+    results2 = load_results(model_name2).drop(columns=["Metric"]).values.tolist()
     print(f"Results2: {results2}")
     ttest = stats.ttest_ind(results1, results2)
     print(f'T-test between {model_name} and {model_name2}')
