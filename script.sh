f22baa119c1b6d2af6e82ec84b1a20cd88a5d9ca
Jeremstym <jeremstym@gmail.com>
2025-02-03 16:06:32 +0100

    Script IRENE cross val 1 seed

--- script.sh
+++ script.sh
@@ -422,9 +422,9 @@ for seed in {42..51}; do
     #     poetry run didactic-runner 'hydra.run.dir=/data/stympopper/didacticWORKSHOP/TEST-Decoupling-FT-2UniFTs-interleaved-InfoNCE-SupCLIP-crossval/fold${fold}/seed${seed}' +experiment=cardinal/xtab-alignment exclude_tabular_attrs=[ht_severity,ht_grade] seed=$seed task/data=tab-13+time-series task/model/encoder=xtab-interleaved-2UniFTs-invert 'data.subsets.train=/home/stympopper/data/CARDINAL/splits/${fold}/train.txt' 'data.subsets.val=/home/stympopper/data/CARDINAL/splits/${fold}/val.txt' 'data.subsets.test=/home/stympopper/data/CARDINAL/splits/${fold}/test.txt' +fold=$fold
     # done
 
-    for fold in {0..4}; do
-        poetry run didactic-runner 'hydra.run.dir=/data/stympopper/didacticWORKSHOP/TEST-FT-Transformer-crossval/fold${fold}/seed${seed}' +experiment=cardinal/xtab exclude_tabular_attrs=[ht_severity,ht_grade] seed=$seed task/data=tab-13+time-series 'data.subsets.train=/home/stympopper/data/CARDINAL/splits/${fold}/train.txt' 'data.subsets.val=/home/stympopper/data/CARDINAL/splits/${fold}/val.txt' 'data.subsets.test=/home/stympopper/data/CARDINAL/splits/${fold}/test.txt' +fold=$fold
-    done
+    # for fold in {0..4}; do
+    #     poetry run didactic-runner 'hydra.run.dir=/data/stympopper/didacticWORKSHOP/TEST-FT-Transformer-crossval/fold${fold}/seed${seed}' +experiment=cardinal/xtab exclude_tabular_attrs=[ht_severity,ht_grade] seed=$seed task/data=tab-13+time-series 'data.subsets.train=/home/stympopper/data/CARDINAL/splits/${fold}/train.txt' 'data.subsets.val=/home/stympopper/data/CARDINAL/splits/${fold}/val.txt' 'data.subsets.test=/home/stympopper/data/CARDINAL/splits/${fold}/test.txt' +fold=$fold
+    # done
 
     # for fold in {0..4}; do
     #     poetry run didactic-runner 'hydra.run.dir=/data/stympopper/didacticWORKSHOP/TEST-Decoupling-FT-interleaved/fold${fold}/seed${seed}' +experiment=cardinal/xtab-alignment exclude_tabular_attrs=[ht_severity,ht_grade] seed=$seed task/data=tab-13+time-series task/model/encoder=xtab-interleaved 'data.subsets.train=/home/stympopper/data/CARDINAL/data/train_cv${fold}.txt' 'data.subsets.val=/home/stympopper/data/CARDINAL/data/val_cv${fold}.txt' 'data.subsets.test=/home/stympopper/data/CARDINAL/data/test_cv${fold}.txt' +fold=$fold task.contrastive_loss_weight=0.0 task.inter_sample_loss_weight=0.0
@@ -457,4 +457,10 @@ for seed in {42..51}; do
 
     # poetry run didactic-runner 'hydra.run.dir=/data/stympopper/didacticWORKSHOP/TEST-Decoupling-FT-2UniFTs-interleaved-NTXonly/seed${seed}' +experiment=cardinal/xtab-alignment exclude_tabular_attrs=[ht_severity,ht_grade] seed=$seed task/data=tab-13+time-series task/model/encoder=xtab-interleaved-2UniFTs-invert task.inter_sample_loss_weight=0.0
 
+done
+
+#! CROSS VALIDATION WITH 1 SEED
+
+for fold in {0..4}; do
+    didactic-runner 'hydra.run.dir=/data/stympopper/didacticWORKSHOP/IRENE-baseline/seed${seed}' +experiment=cardinal/xtab 'task.predict_losses={ht_severity:{_target_:torch.nn.CrossEntropyLoss}}' exclude_tabular_attrs=[ht_severity,ht_grade] seed=42 task/data=tab-13+time-series task/model/encoder=baseline-irene task.embed_dim=768 'data.subsets.train=/home/stympopper/data/CARDINAL/splits/${fold}/train.txt' 'data.subsets.val=/home/stympopper/data/CARDINAL/splits/${fold}/val.txt' 'data.subsets.test=/home/stympopper/data/CARDINAL/splits/${fold}/test.txt' +fold=$fold
 done
