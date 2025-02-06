bd0cd45e32fa0d565ec961ef63a32edb67c7ab45
Jeremstym <jeremstym@gmail.com>
2025-02-06 13:53:57 +0100

    Create and config big_mlp

--- didactic/models/decoupling.py
+++ didactic/models/decoupling.py
@@ -43,6 +43,13 @@ class DecouplingModule(nn.Module):
                 ts_input_size=ts_input_size,
                 ts_proj_size=ts_proj_size,
             )
+        elif decoupling_method == "big_mlp":
+            self.decoupling = BigMLPDecoupling(
+                tab_input_size=tab_input_size,
+                tab_proj_size=tab_proj_size,
+                ts_input_size=ts_input_size,
+                ts_proj_size=ts_proj_size,
+            )
 
     def forward(self, ts_tokens: Tensor, tab_tokens: Tensor) -> Tensor:
         return self.decoupling(ts_tokens, tab_tokens)
@@ -84,6 +91,38 @@ class MLPDecoupling(nn.Module):
         )
         self.mlp_tab = nn.Sequential(
             nn.Linear(tab_input_size, tab_proj_size),
+            nn.GELU(),
+            nn.Linear(tab_proj_size, tab_proj_size),
+        )
+
+    def forward(self, ts_tokens: Tensor, tab_tokens: Tensor) -> Tensor:
+        ts_tokens = self.mlp_ts(ts_tokens)
+        tab_tokens = self.mlp_tab(tab_tokens)
+        tab_tokens_specific = tab_tokens.reshape(tab_tokens.shape[0], -1, self.tab_split)[:,::2,:]
+        tab_tokens_shared = tab_tokens.reshape(tab_tokens.shape[0], -1, self.tab_split)[:,1::2,:]
+        return ts_tokens, tab_tokens_specific, tab_tokens_shared
+
+class BigMLPDecoupling(nn.Module):
+    def __init__(
+        self,
+        tab_input_size: int,
+        tab_proj_size: int,
+        ts_input_size: int,
+        ts_proj_size: int,
+    ):
+        super().__init__()
+        self.tab_split = int(tab_proj_size/2)
+        self.mlp_ts = nn.Sequential(
+            nn.Linear(ts_input_size, ts_proj_size),
+            nn.GELU(),
+            nn.Linear(ts_proj_size, ts_proj_size),
+            nn.GELU(),
+            nn.Linear(ts_proj_size, ts_proj_size),
+        )
+        self.mlp_tab = nn.Sequential(
+            nn.Linear(tab_input_size, tab_proj_size),
+            nn.GELU(),
+            nn.Linear(tab_proj_size, tab_proj_size),
             nn.GELU(),
             nn.Linear(tab_proj_size, tab_proj_size),
         )
