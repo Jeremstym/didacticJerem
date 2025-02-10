cbf5dcfff53f3254c044e7a44235fb030d0c8d7c
Jeremstym <jeremstym@gmail.com>
2025-02-10 01:55:10 +0100

    Fix variable names

--- didactic/models/transformer.py
+++ didactic/models/transformer.py
@@ -4853,8 +4853,8 @@ class FT_Interleaved_2UniFTs_Inverted_TS(nn.Module):
             return ts_tokens.mean(dim=1), tab_tokens_unique.mean(dim=1), tab_tokens_shared.mean(dim=1)
 
         # x = torch.cat([ts_tokens, tab_tokens_unique, tab_tokens_shared, cls_tokens.unsqueeze(1)], dim=1)
-        x = torch.cat([ts_tokens_unique, cls_tokens.unsqueeze(1)], dim=1)
-        x_context = torch.cat([ts_tokens_shared, tab_tokens,], dim=1)
+        x = torch.cat([tab_tokens_unique, cls_tokens.unsqueeze(1)], dim=1)
+        x_context = torch.cat([tab_tokens_shared, ts_tokens,], dim=1)
 
         for layer_idx, block in enumerate(self.blocks):
             if "cross_attention" not in block:
