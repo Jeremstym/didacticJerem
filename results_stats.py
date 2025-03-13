9362904ad57ca7af329f9206893f774b9d2d7cd4
Jeremstym <jeremstym@gmail.com>
2025-03-13 14:42:55 +0100

    Adapt any metrics

--- results_stats.py
+++ results_stats.py
@@ -3,7 +3,7 @@ from glob import glob
 import click
 from scipy import stats
 
-def load_results(model_name: str) -> pd.DataFrame:
+def load_results(model_name: str, metric: str) -> pd.DataFrame:
     path = '/data/stympopper/didacticWORKSHOP/' + model_name + '/seed[0-9][0-9]/predictions/test_categorical_scores.csv'
     files = glob(path)
     if not files:
@@ -11,7 +11,7 @@ def load_results(model_name: str) -> pd.DataFrame:
     results = pd.DataFrame()
     for file in files:
         df = pd.read_csv(file, index_col=0) 
-        df = df.loc[["auroc"]]["ht_severity_prediction"].reset_index().rename(columns={"index": "Metric"})
+        df = df.loc[[metric]]["ht_severity_prediction"].reset_index().rename(columns={"index": "Metric"})
         results = pd.concat([results, df], axis=0)
     results = results.reset_index(drop=True)
     print(results)
@@ -25,11 +25,12 @@ def load_results(model_name: str) -> pd.DataFrame:
 @click.command()
 @click.option("--model_name", help="Name of the model to load results from")
 @click.option("--model_name2", help="Name of the model to load results from")
-def ttests(model_name: str, model_name2: str) -> pd.DataFrame:
-    results1 = load_results(model_name).drop(columns=["Metric"]).values.squeeze(axis=1)
+@click.option("--metric", help="Metric to load")
+def ttests(model_name: str, model_name2: str, metric: str) -> pd.DataFrame:
+    results1 = load_results(model_name, metric).drop(columns=["Metric"]).values.squeeze(axis=1)
     # print(f"df1 is {load_results(model_name).drop(columns=['Metric'])}")
     # print(f"auroc1 is {results1}")
-    results2 = load_results(model_name2).drop(columns=["Metric"]).values.squeeze(axis=1)
+    results2 = load_results(model_name2, metric).drop(columns=["Metric"]).values.squeeze(axis=1)
     # print(f"df2 is {load_results(model_name2).drop(columns=['Metric'])}")
     # print(f"auroc2 is {results2}")
     ttest = stats.ttest_rel(results1, results2)
