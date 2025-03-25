30b3509c4d29ae4ea50abbbe977134b3bdfe9481
Jeremstym <jeremstym@gmail.com>
2025-03-25 16:14:08 +0100

    Convert tensor to list to serializee

--- didactic/models/tabular.py
+++ didactic/models/tabular.py
@@ -296,7 +296,7 @@ class TabularLinearSerializer(nn.Module):
         Raises:
             AssertionError: if the described requirements for the inputs are not met.
         """
-        tabular_attrs = {attr: tabular_attrs[attr].item() for attr in tabular_attrs}
+        tabular_attrs = {attr: tabular_attrs[attr].tolist()[0] for attr in tabular_attrs}
         return json.dumps(tabular_attrs).replace("\na", "[SEP]")
 
 class TabularLinearEmbedding(nn.Module):
