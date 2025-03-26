78c7c13266b7771bc7cebf3452829972ac9d05e2
Jeremstym <jeremstym@gmail.com>
2025-03-26 13:45:39 +0100

    Correct and fix config

--- didactic/models/llms.py
+++ didactic/models/llms.py
@@ -189,7 +189,7 @@ class TabBioBERT(nn.Module):
         pass
 
 class TaBERTModel(nn.Module):
-    def __init__(self, model_name='bert-base-uncased'):
+    def __init__(self, model_name):
         super(BertModel, self).__init__()
         self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
         self.model = AutoModelForMaskedLM.from_pretrained(model_name, num_labels=3)
