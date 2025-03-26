3e3346fe5f02837ef48a26de16453256a048cc39
Jeremstym <jeremstym@gmail.com>
2025-03-26 16:48:13 +0100

    Remove none output

--- didactic/models/tabular.py
+++ didactic/models/tabular.py
@@ -299,7 +299,7 @@ class TabularLinearSerializer(nn.Module):
         tabular_attrs = {str(attr.value): tabular_attrs[attr].tolist()[0] for attr in tabular_attrs}
         serialized_dict = '[SEP]'.join(f"{k}: {v}" for k, v in tabular_attrs.items())
         print(serialized_dict)
-        return serialized_dict, None 
+        return serialized_dict 
 
 class TabularLinearEmbedding(nn.Module):
     """Combines `LinearEmbeddings` and `CategoricalEmbeddings`.
