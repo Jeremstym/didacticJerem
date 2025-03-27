92b651dae2ba653be7331b230b2c573b6a72369c
Jeremstym <jeremstym@gmail.com>
2025-03-27 17:36:15 +0100

    Use dictionary directly

--- didactic/models/llms.py
+++ didactic/models/llms.py
@@ -151,13 +151,14 @@ class BertTabTokenizer(BertTokenizer):
 
     def _serialize(self, batch_tabular_attrs):
         # tabular_attrs = {attr: tabular_attrs[attr] for attr in tabular_attrs}
-        # inputs_text = '[SEP]'.join(f"{k}: {v}" for k, v in batch_tabular_attrs.items())
-        # return inputs_text
-        serialized_batch = [
-            '[SEP]'.join(f"{k}: {v}" for k, v in sample.items())
-            for sample in batch_tabular_attrs
-        ]
-        return serialized_batch
+        inputs_text = '[SEP]'.join(f"{k}: {v}" for k, v in batch_tabular_attrs.items())
+        print(f"inputs_text: {inputs_text}")
+        return inputs_text
+        # serialized_batch = [
+        #     '[SEP]'.join(f"{k}: {v}" for k, v in sample.items())
+        #     for sample in batch_tabular_attrs
+        # ]
+        # return serialized_batch
 
     def _tokenize(self, text):
         # add a special token [NUM] ahead of numerical values
