e8974686d90659c08db07e988ce0d51fb5127bcf
Jeremstym <jeremstym@gmail.com>
2025-05-13 18:12:00 +0200

    Monitor ts_notna_mask

--- didactic/tasks/cardiac_multimodal_representation.py
+++ didactic/tasks/cardiac_multimodal_representation.py
@@ -514,6 +514,8 @@ class CardiacMultimodalRepresentationTask(SharedStepsTask):
             time_series_notna_mask = torch.full(
                 time_series_attrs_tokens.shape[:2], True, device=time_series_attrs_tokens.device
             )
+            print(f"Time series notna mask shape: {time_series_notna_mask.shape}")
+            raise Exception("stop")
             notna_mask.append(time_series_notna_mask)
         
         if tabular_attrs:
@@ -729,11 +731,11 @@ class CardiacMultimodalRepresentationTask(SharedStepsTask):
     def _shared_step(self, batch: PatientData, batch_idx: int) -> Dict[str, Tensor]:
         # Extract tabular and time-series attributes from the batch
         tabular_attrs = {attr: attr_data for attr, attr_data in batch.items() if attr in self.hparams.tabular_attrs}
-        time_series_attrs, time_series_attrs_notna_mask = filter_time_series_attributes(
+        time_series_attrs, time_series_notna_mask = filter_time_series_attributes(
             batch, views=self.hparams.views, attrs=self.hparams.time_series_attrs
         )
 
-        print(f'notna mask: {time_series_attrs_notna_mask}')
+        print(f'notna mask: {time_series_notna_mask}')
         raise Exception("stop here")
         in_tokens, avail_mask = self.tokenize(tabular_attrs, time_series_attrs)  # (N, S, E), (N, S)
         
