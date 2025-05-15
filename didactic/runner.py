65b4c0e12a0179a932c76fd1b4d732bd49431e15
Jeremstym <jeremstym@gmail.com>
2025-05-15 12:09:56 +0200

    Replace folder and rename config path

--- didactic/runner.py
+++ didactic/runner.py
@@ -1,14 +1,14 @@
 import comet_ml  # noqa
 import hydra
 from omegaconf import DictConfig
-from vital.runner import VitalRunner
+from dataprocessing.runner import VitalRunner
 
 
 class DidacticRunner(VitalRunner):
     """Entry-point for a `VitalRunner` that adds the `didactic` config dir to the Hydra search path."""
 
     @staticmethod
-    @hydra.main(version_base=None, config_path="config", config_name="vital_default")
+    @hydra.main(version_base=None, config_path="config", config_name="default")
     def run_system(cfg: DictConfig) -> None:  # noqa: D102
         VitalRunner.run_system(cfg)
 
